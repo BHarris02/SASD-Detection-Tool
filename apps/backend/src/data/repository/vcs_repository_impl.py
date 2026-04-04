0458c25f72304579e429977ec2971d3118a59a92
BHarris02 <dev.blake.harris@gmail.com>
2026-04-04 14:29:49 +0100

    fix: make `GitHubVCSRepository.api_service` private

--- apps/backend/src/data/repository/vcs_repository_impl.py
+++ apps/backend/src/data/repository/vcs_repository_impl.py
@@ -15,11 +15,11 @@ class GitHubVCSRepository(VCSRepository):
     A concrete `VCSRepository` implemented using GitHub.
     """
     def __init__(self, api_service: VCSApiService) -> None:
-        self.api_service = api_service
+        self._api_service = api_service
 
     def get_commits(self, repo_url: str) -> list[Commit]:
         try:
-            commit_dtos = self.api_service.fetch_commits(repo_url)
+            commit_dtos = self._api_service.fetch_commits(repo_url)
             commits = [ commit_dto_to_domain(dto) for dto in commit_dtos ]
             return commits
         except Exception as e:
@@ -27,7 +27,7 @@ class GitHubVCSRepository(VCSRepository):
 
     def get_issues(self, repo_url: str) -> list[Issue]:
         try:
-            issue_dtos = self.api_service.fetch_issues(repo_url)
+            issue_dtos = self._api_service.fetch_issues(repo_url)
             issues = [ issue_dto_to_domain(dto) for dto in issue_dtos ]
             return issues
         except Exception as e:
@@ -35,7 +35,7 @@ class GitHubVCSRepository(VCSRepository):
 
     def get_file_content(self, repo_url: str, file_path: str) -> FileContent:
         try:
-            content_dto = self.api_service.fetch_file_content(repo_url, file_path)
+            content_dto = self._api_service.fetch_file_content(repo_url, file_path)
             content = file_content_dto_to_domain(content_dto)
             return content
         except Exception as e:
