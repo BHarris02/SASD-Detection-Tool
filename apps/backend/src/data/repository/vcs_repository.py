e55465b10346a5141bf1e370db98eaa4cdb7a869
BHarris02 <dev.blake.harris@gmail.com>
2026-04-04 14:14:36 +0100

    implement data layer `GitHubVCSRepository`

---
+++ apps/backend/src/data/repository/vcs_repository.py
@@ -0,0 +1,45 @@
+"""
+Concrete implementations of VCSRepository contract exposed in `domain` layer.
+"""
+from apps.backend.src.data.mapper.vcs_entity_mapper import (
+    commit_dto_to_domain,
+    issue_dto_to_domain,
+    file_content_dto_to_domain
+)
+from apps.backend.src.data.remote.vcs.vcs_api_service import VCSApiService
+from apps.backend.src.domain.entity.vcs import Commit, FileContent, Issue, RepositoryItem
+from apps.backend.src.domain.repository.vcs_repository import VCSRepository
+
+class GitHubVCSRepository(VCSRepository):
+    """
+    A concrete `VCSRepository` implemented using GitHub.
+    """
+    def __init__(self, api_service: VCSApiService) -> None:
+        self.api_service = api_service
+
+    def get_commits(self, repo_url: str) -> list[Commit]:
+        try:
+            commit_dtos = self.api_service.fetch_commits(repo_url)
+            commits = [ commit_dto_to_domain(dto) for dto in commit_dtos ]
+            return commits
+        except Exception as e:
+            raise e
+
+    def get_issues(self, repo_url: str) -> list[Issue]:
+        try:
+            issue_dtos = self.api_service.fetch_issues(repo_url)
+            issues = [ issue_dto_to_domain(dto) for dto in issue_dtos ]
+            return issues
+        except Exception as e:
+            raise e
+
+    def get_file_content(self, repo_url: str, file_path: str) -> FileContent:
+        try:
+            content_dto = self.api_service.fetch_file_content(repo_url, file_path)
+            content = file_content_dto_to_domain(content_dto)
+            return content
+        except Exception as e:
+            raise e
+
+    def get_repository_structure(self, repo_url: str) -> list[RepositoryItem]:
+        ...
