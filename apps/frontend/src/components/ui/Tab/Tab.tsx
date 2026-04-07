794d72966ee5084a4dede892f24c14950823919c
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-07 16:03:42 +0100

    add `Tab` component

--- apps/frontend/src/components/ui/Tab/Tab.tsx
+++ apps/frontend/src/components/ui/Tab/Tab.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 
-export default function Tab() {
+export default function Tab(tabs) {
+    // state
+    const [activeTab, setActiveTab] = useState<number>(0);
+
     return (
         <div className="tab">
             <div className="tab-headers">
-
+                { tabs.map((tab, index) => (
+                    <button
+                        key={index}
+                        onClick={() => setActiveTab(index)}
+                    >
+                        { tab.label }
+                    </button>
+                ))}
             </div>
             <div className="tab-content">
-
+                <div>{ tabs[activeTab].content }</div>
             </div>
         </div>
     );
