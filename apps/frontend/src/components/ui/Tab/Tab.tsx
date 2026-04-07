82b33119c3387e84950845a1a610b9a19be74ac3
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-07 10:43:20 +0100

    add `Tab` component

---
+++ apps/frontend/src/components/ui/Tab/Tab.tsx
@@ -0,0 +1,13 @@
+
+export default function Tab() {
+    return (
+        <div className="tab">
+            <div className="tab-headers">
+
+            </div>
+            <div className="tab-content">
+
+            </div>
+        </div>
+    );
+}
