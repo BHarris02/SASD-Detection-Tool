59f46b236202c9aab2a80a40ac5325040655df9f
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-07 16:03:23 +0100

    add `FileViewer` component

--- apps/frontend/src/components/ui/FileViewer/FileViewer.tsx
+++ apps/frontend/src/components/ui/FileViewer/FileViewer.tsx
@@ -1,12 +1,26 @@
 import styles from './FileViewer.module.css';
 
-export default function FileViewer() {
+interface FileViewerProps {
+    content: string,
+    setSelectedCode: (text: string) => void
+}
+
+export default function FileViewer({ content, setSelectedCode }: FileViewerProps) {
+    // handlers
+    const handleTextSelection = () => {
+        const selection = window.getSelection();
+        const selectedText = selection.toString();
+        setSelectedCode(selectedText);
+    }
+
     return (
         <div className={styles["file-viewer"]}>
             <h2>File Viewer</h2>
             <textarea 
                 className={styles["file-viewer-textarea"]}
                 readOnly
+                value={content}
+                onMouseUp={handleTextSelection}
             />
         </div>
     );
