491749425fd312fe837ab4eba8c1896ddcd21ebc
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-07 10:30:55 +0100

    Add `RepoFileTree` component

---
+++ apps/frontend/src/components/sections/RepoFileTree/RepoFileTree.tsx
@@ -0,0 +1,9 @@
+import styles from './RepoFileTree.module.css';
+
+export default function RepoFileTree() {
+    return (
+        <div className={styles["repo-file-tree"]}>
+            <p>No repository loaded.</p>
+        </div>
+    );
+}
