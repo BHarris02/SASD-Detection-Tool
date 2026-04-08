15f9b66b295fa2c1573b4ac073d92fa996008b8c
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-08 15:53:46 +0100

    WIP

--- apps/frontend/src/components/sections/FileBrowser/FileTree/FileTree.tsx
+++ apps/frontend/src/components/sections/FileBrowser/FileTree/FileTree.tsx
@@ -1,9 +1,37 @@
+import type { FileTreeNode } from '@/types/vcs';
 import styles from './FileTree.module.css';
+import { FileTreeFolderNode } from './FileTreeFolderNode';
+import { FileTreeFileNode } from './FileTreeFileNode';
 
-export default function FileTree() {
+interface FileTreeProps {
+    tree: FileTreeNode[] | null;
+}
+
+export default function FileTree({ tree }: FileTreeProps) {
+    // utils
+    function renderTree(node: FileTreeNode) {
+        if (!node) return null;
+
+        if (node.type === "folder") {
+            return <FileTreeFolderNode
+                node={node}
+                renderTree={renderTree}
+            />
+        }
+
+        return <FileTreeFileNode
+            node={node}
+            onFileClick={null}  // temp
+        />
+    }
+
     return (
         <div className={styles.fileTree}>
-            <p>No repository loaded.</p>
+            { tree? (
+                <ul>{ tree.map((node) => renderTree(node)) }</ul>
+            ) : (
+                <p>No repository loaded.</p>
+            )}
         </div>
     );
 }
