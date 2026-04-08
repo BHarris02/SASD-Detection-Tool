7c346fc7d184182f533b60a5f2f6db1280faffc4
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-08 11:38:44 +0100

    add updated `FileBrowser` component

---
+++ apps/frontend/src/components/sections/FileBrowser/FileBrowser.tsx
@@ -0,0 +1,14 @@
+import styles from './FileBrowser.module.css';
+
+export default function FileBrowser() {
+    return (
+        <section className={styles.fileBrowser}>
+            <div>
+                
+            </div>
+            <div>
+
+            </div>
+        </section>
+    );
+}
