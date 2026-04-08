e43127bee0f4407304169afe8bc151fa0592d0d9
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-08 12:23:12 +0100

    update `Dashboard` page to render `FileBrowser`

--- apps/frontend/src/pages/Dashboard/Dashboard.tsx
+++ apps/frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,91 +1,12 @@
-import LoadingSpinner from "@/components/ui/LoadingSpinner";
-import RepoInput from "@/components/sections/RepoInput";
-import RepoFileTree from "@/components/sections/RepoFileTree";
-import Tab from "@/components/ui/Tab";
-import FileViewer from "@/components/ui/FileViewer";
-import BackendActions from "@/components/sections/BackendActions";
-import Modal from "@/components/ui/Modal";
-import { useDashboard } from "@/hooks/useDashboard";
+import FileBrowser from '@/components/sections/FileBrowser';
+import styles from './Dashboard.module.css';
+import Workspace from '@/components/sections/Workspace';
 
 export default function Dashboard() {
-    const {
-        loading,
-        repoUrl,
-        setRepoUrl,
-        handleFetchRepoStructure,
-        fileTree,
-        selectedFilePath,
-        handleFileClick,
-        showTabs,
-        scanResults,
-        fileViewerContent,
-        setSelectedCode,
-        handleScanIssues,
-        handleScanRepository,
-        handleScanCommits,
-        handleScanFileComments,
-        handleScanMethodComments,
-        showModal,
-        setShowModal
-    } = useDashboard();
-
     return (
-        <div className="dashboard">
-            <LoadingSpinner loading={loading} />
-
-            <h1>SASD Detection Tool: Web</h1>
-
-            <RepoInput
-                repoUrl={repoUrl}
-                setRepoUrl={setRepoUrl}
-                fetchRepoStructure={handleFetchRepoStructure}
-            />
-            <RepoFileTree
-                fileTree={fileTree}
-                selectedFilePath={selectedFilePath}
-                onFileClick={handleFileClick}
-            />
-
-            <div>
-                { showTabs && scanResults ? (
-                    <Tab
-                        tabs={[
-                            {
-                                label: "Commit Analysis",
-                                content: null
-                            },
-                            {
-                                label: "Issue Analysis",
-                                content: null
-                            },
-                            {
-                                label: "Code Analysis",
-                                content: null
-                            }
-                        ]}
-                    />
-                ) : (
-                    <FileViewer
-                        content={fileViewerContent}
-                        setSelectedCode={setSelectedCode}
-                    />
-                )}
-            </div>
-
-            <BackendActions
-                onScanCommits={() => handleScanCommits}
-                onScanIssues={() => handleScanIssues}
-                onScanCodeComments={() => handleScanMethodComments}
-                onScanFileComments={() => handleScanFileComments}
-                onScanRepository={() => handleScanRepository(repoUrl)}
-            />
-            <Modal
-                show={showModal}
-                onClose={() => setShowModal(false)}
-                title="Analysis Result"
-            >
-                <p>No available data.</p>
-            </Modal>
-        </div>
+        <main className={styles.dashboard}>
+            <FileBrowser />
+            <Workspace />
+        </main>
     );
 }
