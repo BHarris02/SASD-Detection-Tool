db21434bfb471274ceae81d49195caa535caf897
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-08 14:52:04 +0100

    add `RepositoryInput` props and form submission handler

--- apps/frontend/src/components/sections/FileBrowser/RepositoryInput/RepositoryInput.tsx
+++ apps/frontend/src/components/sections/FileBrowser/RepositoryInput/RepositoryInput.tsx
@@ -1,11 +1,31 @@
 import styles from './RepositoryInput.module.css';
 
-export default function RepositoryInput() {
+interface RepositoryInputProps {
+    repoUrl: string;
+    setRepoUrl: (url: string) => void;
+    handleFetchRepoStructure: (repoUrl: string) => void;
+}
+
+export default function RepositoryInput({ repoUrl, setRepoUrl, handleFetchRepoStructure }: RepositoryInputProps) {
+    // handlers
+    const handleSubmit = (event: React.SubmitEvent) => {
+        event.preventDefault();
+
+        if (!repoUrl.trim()) {
+            alert("Please enter a repository URL.");
+            return;
+        }
+
+        handleFetchRepoStructure(repoUrl);
+    }
+
     return (
-        <form className={styles.repositoryInputForm}>
+        <form onSubmit={handleSubmit} className={styles.repositoryInputForm}>
             <input
                 type="text"
                 placeholder="Enter Repository URL ('owner/repository-name')"
+                value={repoUrl}
+                onChange={ (event) => setRepoUrl(event.target.value) }
             />
             <button type="submit">Load Repository Tree</button>
         </form>
