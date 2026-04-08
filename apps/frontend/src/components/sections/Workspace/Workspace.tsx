6be083bea609ede6131790fbc73bce9d0ae10989
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-08 11:39:02 +0100

    add `Workspace` component

---
+++ apps/frontend/src/components/sections/Workspace/Workspace.tsx
@@ -0,0 +1,14 @@
+import styles from './WorkspacePanel.module.css';
+
+export default function Workspace() {
+    return (
+        <section className={styles.workspace}>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </section>
+    );
+}
