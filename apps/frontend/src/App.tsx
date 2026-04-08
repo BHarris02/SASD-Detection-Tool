2854569a70d889617da4b0d294cc7ab2d25579d6
BHarris02 <blaharris+deloitte@deloitte.co.uk>
2026-04-08 11:13:31 +0100

    render `Navbar`

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Dashboard from '@/pages/Dashboard';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Dashboard />
     </>
   )
