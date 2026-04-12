66736d454e28839b7b97ec66714bb56c35d5892e
Blake Harris <118061899+BHarris02@users.noreply.github.com>
2026-04-12 17:34:22 +0100

    Revert "Revert "Incomplete backend refactor""

---
+++ apps/backend/src/data/mapper/vcs_entity_mapper.py
@@ -0,0 +1,44 @@
+"""
+Utility mapper functions to map from `data` DTO to `domain` business entity.
+"""
+from data.client.vcs.dtos import (
+    CommitDto, IssueDto, IssueLabelDto, FileContentDto
+)
+from domain.entity.vcs import (
+    Commit, Issue, IssueLabel, FileContent
+)
+
+def commit_dto_to_domain(dto: CommitDto) -> Commit:
+    """
+    Map `CommitDto` to `Commit`.
+    """
+    return Commit(
+        message=dto.message
+    )
+
+def _issue_label_dto_to_domain(dto: IssueLabelDto) -> IssueLabel:
+    return IssueLabel(
+        name=dto.name,
+        description=dto.description
+    )
+
+def issue_dto_to_domain(dto: IssueDto) -> Issue:
+    """
+    Map `IssueDto` to `Issue`.
+    """
+    return Issue(
+        title=dto.title,
+        description=dto.description,
+        labels=[
+            _issue_label_dto_to_domain(label_dto)
+            for label_dto in dto.labels
+        ] if dto.labels else []
+    )
+
+def file_content_dto_to_domain(dto: FileContentDto) -> FileContent:
+    """
+    Map `FileContentDto` to `FileContent`.
+    """
+    return FileContent(
+        dto.content
+    )
---
+++ apps/backend/src/di/module/usecase.py
@@ -0,0 +1,108 @@
+"""
+Wire together `domain` layer dependencies and exposes them via `@provider` decorated methods.
+"""
+from injector import Module, provider, singleton
+
+from domain.gateway.nlp_gateway_api import NLPGateway
+from domain.repository.vcs_repository_api import VCSRepository
+from domain.usecase.analysis.api import (
+    AnalyzeCommitsUseCase,
+    AnalyzeIssuesUseCase,
+    AnalyzeCommentsUseCase,
+    AnalyzeFileCommentsUseCase,
+    AnalyzeRepositoryUseCase
+)
+from domain.usecase.analysis.impl import (
+    AnalyzeCommitsUseCaseImpl,
+    AnalyzeIssuesUseCaseImpl,
+    AnalyzeCommentsUseCaseImpl,
+    AnalyzeFileCommentsUseCaseImpl,
+    AnalyzeRepositoryUseCaseImpl
+)
+from domain.usecase.vcs.api import (
+    GetFileContentUseCase, GetRepositoryStructureUseCase
+)
+from domain.usecase.vcs.impl import (
+    GetFileContentUseCaseImpl, GetRepositoryStructureUseCaseImpl
+)
+
+class UsecaseModule(Module):
+    """
+    Wire together `domain` layer dependencies and exposes them via `@provider` decorated methods.
+    """
+
+    @provider
+    @singleton
+    def provide_analyze_commits(
+        self,
+        nlp_gateway: NLPGateway,
+        vcs_repo: VCSRepository
+    ) -> AnalyzeCommitsUseCase:
+        """
+        Provide a singleton instance of `AnalyzeCommitsUseCase`.
+        """
+        return AnalyzeCommitsUseCaseImpl(nlp_gateway=nlp_gateway, vcs_repo=vcs_repo)
+
+    @provider
+    @singleton
+    def provide_analyze_issues(
+        self,
+        nlp_gateway: NLPGateway,
+        vcs_repo: VCSRepository
+    ) -> AnalyzeIssuesUseCase:
+        """Provide a singleton instance of `AnalyzeIssuessUseCase`."""
+        return AnalyzeIssuesUseCaseImpl(nlp_gateway=nlp_gateway, vcs_repo=vcs_repo)
+
+    @provider
+    @singleton
+    def provide_analyze_comments(
+        self,
+        nlp_gateway: NLPGateway,
+    ) -> AnalyzeCommentsUseCase:
+        """Provide a singleton instance of `AnalyzeCommentssUseCase`."""
+        return AnalyzeCommentsUseCaseImpl(nlp_gateway=nlp_gateway)
+
+    @provider
+    @singleton
+    def provide_analyze_file_comments(
+        self,
+        nlp_gateway: NLPGateway,
+        vcs_repo: VCSRepository,
+    ) -> AnalyzeFileCommentsUseCase:
+        """Provide a singleton instance of `AnalyzeFileCommentsUseCase`."""
+        return AnalyzeFileCommentsUseCaseImpl(nlp_gateway=nlp_gateway, vcs_repo=vcs_repo)
+
+    @provider
+    @singleton
+    def provide_analyze_repository(
+        self,
+        analyze_commits: AnalyzeCommitsUseCase,
+        analyze_issues: AnalyzeIssuesUseCase,
+        analyze_file_comments: AnalyzeFileCommentsUseCase,
+        get_repository_structure: GetRepositoryStructureUseCase,
+    ) -> AnalyzeRepositoryUseCase:
+        """Provide a singleton instance of `AnalyzeRepositoryUseCase`."""
+        return AnalyzeRepositoryUseCaseImpl(
+            analyze_commits=analyze_commits,
+            analyze_issues=analyze_issues,
+            analyze_file_content=analyze_file_comments,
+            get_repository_structure=get_repository_structure,
+        )
+
+    @provider
+    @singleton
+    def provide_get_file_content_use_case(
+        self,
+        repo: VCSRepository
+    ) -> GetFileContentUseCase:
+        """Provide a singleton instance of `GetFileContentUseCase`."""
+        return GetFileContentUseCaseImpl(repo)
+
+    @provider
+    @singleton
+    def provide_get_repository_structure_use_case(
+        self,
+        repo: VCSRepository
+    ) -> GetRepositoryStructureUseCase:
+        """Provide a singleton instance of `GetRepositoryStructureUseCase`."""
+        return GetRepositoryStructureUseCaseImpl(repo)
