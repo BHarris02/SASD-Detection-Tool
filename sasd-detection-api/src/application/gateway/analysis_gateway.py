691fdc6a29719ba8e6de755470485cc6c1feae69
BHarris02 <dev.blake.harris@gmail.com>
2026-05-04 11:20:38 +0100

    feat: enhance analysis and VCS gateways with type annotations and new analysis failure handling

--- sasd-detection-api/src/application/gateway/analysis_gateway.py
+++ sasd-detection-api/src/application/gateway/analysis_gateway.py
@@ -2,14 +2,18 @@
 Analysis gateway interface
 """
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import List
+
+from src.domain.value_object.analysis import AnalysisBatch
+from src.domain.entity.common import Artefact
 
 class AnalysisGateway(ABC):
     """
     Analysis gateway
     """
     @abstractmethod
-    def analyse_artefacts(self, artefact: Any) -> Any:
+    def analyse_artefacts(self, artefacts: List[Artefact]) -> AnalysisBatch:
         """
-        Analyse an artefact for self-admitted security debt
+        Analyse artefacts for self-admitted security debt.
+        Return partial success results
         """
--- sasd-detection-api/src/application/gateway/vcs_gateway.py
+++ sasd-detection-api/src/application/gateway/vcs_gateway.py
@@ -2,26 +2,32 @@
 Version Control System gateway interface
 """
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import List
+
+from src.domain.entity.vcs import (
+    CommitArtefact,
+    IssueArtefact,
+    PullRequestArtefact
+)
 
 class VcsGateway(ABC):
     """
     VCS Gateway
     """
     @abstractmethod
-    def fetch_commits(self, repository_owner: str, repository_name: str) -> Any:
+    def fetch_commits(self, repository_owner: str, repository_name: str) -> List[CommitArtefact]:
         """
         Fetch commits from data source
         """
 
     @abstractmethod
-    def fetch_issues(self, repository_owner: str, repository_name: str) -> Any:
+    def fetch_issues(self, repository_owner: str, repository_name: str) -> List[IssueArtefact]:
         """
         Fetch issues from data source
         """
 
     @abstractmethod
-    def fetch_pull_requests(self, repository_owner: str, repository_name: str) -> Any:
+    def fetch_pull_requests(self, repository_owner: str, repository_name: str) -> List[PullRequestArtefact]:
         """
         Fetch pull requests from data source
         """
