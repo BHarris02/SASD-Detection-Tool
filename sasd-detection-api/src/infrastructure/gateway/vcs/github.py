98df1cb142a2b81dc8cff6d03710c5475907e22f
BHarris02 <dev.blake.harris@gmail.com>
2026-05-05 15:49:22 +0100

    refactor: replace _validate_artefacts with _filter_malformed in GitHubVcsGateway methods

--- sasd-detection-api/src/infrastructure/gateway/vcs/github.py
+++ sasd-detection-api/src/infrastructure/gateway/vcs/github.py
@@ -25,7 +25,7 @@ class GitHubVcsGateway(VcsGateway):
         """
         commit_dtos = self._client.fetch_commits(repository_owner, repository_name)
         commits =  [commit_dto_to_domain(dto) for dto in commit_dtos]
-        return self._validate_artefacts(commits)
+        return self._filter_malformed(commits)
 
     def fetch_issues(self, repository_owner, repository_name):
         """
@@ -33,15 +33,15 @@ class GitHubVcsGateway(VcsGateway):
         """
         issue_dtos = self._client.fetch_issues(repository_owner, repository_name)
         issues = [issue_dto_to_domain(dto) for dto in issue_dtos]
-        return self._validate_artefacts(issues)
+        return self._filter_malformed(issues)
 
     def fetch_pull_requests(self, repository_owner, repository_name):
         """
         Fetch PRs via GitHub API call
         """
         pr_dtos = self._client.fetch_pull_requests(repository_owner, repository_name)
-        prs =  [pull_request_dto_to_domain(dto) for dto in pr_dtos]
-        return self._validate_artefacts(prs)
+        prs = [pull_request_dto_to_domain(dto) for dto in pr_dtos]
+        return self._filter_malformed(prs)
 
-    def _validate_artefacts(self, artefacts: Sequence[Artefact]) -> Sequence[Artefact]:
+    def _filter_malformed(self, artefacts: Sequence[Artefact]) -> Sequence[Artefact]:
         return [artefact for artefact in artefacts if artefact is not None]
