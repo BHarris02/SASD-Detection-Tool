700d1a572a78c91a8107e7acabb56f40b51b7c7c
BHarris02 <dev.blake.harris@gmail.com>
2026-05-21 09:05:29 +0100

    chore: remove reference backend code

--- apps/backend_OLD/src/data/client/nlp/nlp_artifact_type.py
+++
@@ -1,9 +0,0 @@
-"""
-Enum constants for NLP API artifact types.
-"""
-from enum import StrEnum
-
-class NLPArtifactType(StrEnum):
-    COMMIT = "commit message"
-    ISSUE = "issue"
-    COMMENT = "code comment"
