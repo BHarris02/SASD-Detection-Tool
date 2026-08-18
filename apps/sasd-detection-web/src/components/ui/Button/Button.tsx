7aadf365d2b1fe98b9374c57da0fb2b4ddfed4d4
BHarris02 <dev.blake.harris@gmail.com>
2026-08-18 16:42:30 +0100

    refactor: Enhance Button component to support onClick and disabled props

--- apps/sasd-detection-web/src/components/ui/Button/Button.tsx
+++ apps/sasd-detection-web/src/components/ui/Button/Button.tsx
@@ -2,10 +2,18 @@ import styles from './Button.module.css';
 
 interface ButtonProps {
     text: string;
+    onClick?: () => void;
+    disabled?: boolean;
 }
 
-export default function Button({ text }: ButtonProps) {
+export default function Button({ text, onClick, disabled }: ButtonProps) {
     return (
-        <button className={styles.button}>{ text }</button>
+        <button
+            className={styles.button}
+            onClick={onClick}
+            disabled={disabled}
+        >
+            { text }
+        </button>
     );
 }
