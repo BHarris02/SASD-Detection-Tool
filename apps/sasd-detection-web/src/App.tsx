cdfe0916d3837f627876734f16dbc8038ab186f4
BHarris02 <dev.blake.harris@gmail.com>
2026-08-19 13:31:03 +0100

    feat: Update App layout and styles for improved structure and responsiveness

--- apps/sasd-detection-web/src/App.tsx
+++ apps/sasd-detection-web/src/App.tsx
@@ -1,16 +1,32 @@
+import { Toaster } from 'react-hot-toast';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import AnalysisPanel from './components/sections/AnalysisPanel';
 import RepoTree from './components/sections/RepoTree';
+import useRepoTree from './hooks/useRepoTree';
+import useFileSelection from './hooks/useFileSelection';
 
 export default function App() {
+  const { repoInput, setRepoInput, fileTree, isLoading, onLoadRepo } = useRepoTree();
+  const { fileContent, onLoadFile } = useFileSelection();
+
   return (
     <>
+    <Toaster />
     <header>
       <Navbar />
     </header>
 
     <main>
-      <RepoTree />
+      <RepoTree
+        repoInput={repoInput}
+        onRepoInputChange={setRepoInput}
+        fileTree={fileTree}
+        isLoading={isLoading}
+        onLoadRepo={() => onLoadRepo(repoInput)}
+        onFileSelect={(path) => onLoadFile(repoInput, path)}
+      />
+      <AnalysisPanel fileContent={fileContent}/>
     </main>
     </>
   )
